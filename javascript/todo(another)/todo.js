dec2bf55af9c490f293c64e89c598f006182792a
Yifan_MacMini <zyfinori@gmail.com>
2018-09-03 23:32:35 +0800

    modify todo.js

--- javascript/todo(another)/todo.js
+++ javascript/todo(another)/todo.js
@@ -2,7 +2,8 @@ var lis = document.querySelectorAll("li");
 for (var i = 0; i < lis.length; i++)
 {
 	lis[i].addEventListener("mouseover", function() {
-		this.classList.add("selected");//this inside eventListener refers to the item or the element that the event was triggered on
+		this.classList.add("selected");
+		//'this' inside eventListener refers to the item or the element that the event was triggered on
 	});
 	lis[i].addEventListener("mouseout", function() {
 		this.classList.remove("selected");
