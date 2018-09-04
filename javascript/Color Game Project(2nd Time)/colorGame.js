0415b4ff64060c6609e719108ef6c762c6f1b34e
Yifan_MacMini <zyfinori@gmail.com>
2018-09-04 23:05:57 +0800

    Modify Color Game Project(2nd)

--- javascript/Color Game Project(2nd Time)/colorGame.js
+++ javascript/Color Game Project(2nd Time)/colorGame.js
@@ -9,10 +9,40 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++)
 {
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function() {
+        //grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor)
+        {
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+        }
+        else
+        {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+        //compare color to pickedColor
+
+    })
+}
+
+function changeColors(color)
+{
+    //loop through all squares
+    for (var i = 0; i < colors.length; i++)
+    {
+        //change each color to match given color
+        squares[i].style.backgroundColor = color;    
+    }
 }
