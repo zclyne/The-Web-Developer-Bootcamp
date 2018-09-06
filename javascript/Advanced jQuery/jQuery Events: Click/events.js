b897dc62e38c42788fdac00ed460e40c45ade622
Yifan_MacMini <zyfinori@gmail.com>
2018-09-06 21:51:23 +0800

    Add Advanced jQuery

---
+++ javascript/Advanced jQuery/jQuery Events: Click/events.js
@@ -0,0 +1,9 @@
+//click()
+//Bind an event handler to the "click" JavaScript event,
+//or trigger that event on an element.
+$("#submit").click(function() {
+    console.log("Another click");
+});
+$("button").click(function() {
+    alert("Someone clicked a button");
+});
